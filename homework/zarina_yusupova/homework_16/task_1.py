d73ac0b9cf6b93c3a6f3964a10784f2ace7792bf
Zarina_Yusupova <zarina1332484@gmail.com>
2025-12-15 16:12:53 +0300

    deleted whitespaces

--- homework/zarina_yusupova/homework_16/task_1.py
+++ homework/zarina_yusupova/homework_16/task_1.py
@@ -15,26 +15,15 @@ db = pymysql.connect(
 )
 cursor = db.cursor()
 query = """
-SELECT 
-    s.name, 
-    s.second_name, 
-    g.title AS group_title, 
-    b.title AS book_title, 
-    sj.title AS subject_title, 
-    l.title AS lesson_title, 
-    m.value AS mark_value
+SELECT s.name, s.second_name, g.title AS group_title, b.title AS book_title, sj.title AS subject_title, 
+l.title AS lesson_title, m.value AS mark_value
 FROM students s
 JOIN `groups` g ON g.id = s.group_id
 JOIN books b ON s.id = b.taken_by_student_id
 JOIN marks m ON s.id = m.student_id
 JOIN lessons l ON m.lesson_id = l.id
 JOIN subjects sj ON l.subject_id = sj.id
-WHERE s.name = %s 
-AND s.second_name = %s 
-AND g.title = %s 
-AND b.title = %s 
-AND sj.title = %s 
-AND l.title = %s
+WHERE s.name = %s AND s.second_name = %s AND g.title = %s AND b.title = %s AND sj.title = %s AND l.title = %s
 AND m.value = %s
 """
 
