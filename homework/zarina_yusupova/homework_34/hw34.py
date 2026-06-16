6487343870ff1f872c4bef8ee68b31353484e5c4
Zarina_Yusupova <zarina1332484@gmail.com>
2026-06-16 20:19:21 +0300

    homework_34 done

--- homework/zarina_yusupova/homework_34/hw34.py
+++ homework/zarina_yusupova/homework_34/hw34.py
@@ -4,8 +4,8 @@ city = input("Введите ваш город: ")
 
 
 def get_temperature(city):
-    url = ('https://api.openweathermap.org/data/2.5/weather?q=' +
-           city + '&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347'
+    url = ('https://api.openweathermap.org/data/2.5/weather?q=' + city + ''
+           '&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347'
            )
     weather_data = requests.get(url).json()
     temperature = round(weather_data['main']['temp'])
